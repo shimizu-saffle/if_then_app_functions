bd28dd211601ea9d25245f7ab513c2e9ca72fcc5
shimizu-saffle <shimizu.saffle@gmail.com>
2021-08-23 20:14:05 +0900

    Cloud Functionsから通知を送れた
    トークンべた書き

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,48 +1,56 @@
 import * as functions from "firebase-functions";
-// セキュリティルールを無視できる管理者権限でアクセスするためのインポートとイニシャライズ
 import * as admin from "firebase-admin";
-admin.initializeApp(functions.config().functions);
+admin.initializeApp(functions.config().firebase);
+export const firestore = admin.firestore();
 
-// イフゼンプランのonCreateをトリガーに Cloud Firestore のイフゼンプランの情報を取得して返す関数
-export const onCreateNotification3 = functions
+// プッシュ通知を送る関数
+const sendPushNotification = function (
+    token: string,
+    title: string,
+    body: string,
+    badge: string
+) {
+    const payload = {
+        notification: {
+            title: title,
+            body: body,
+            badge: badge,
+            sound: "default",
+        },
+    };
+    const option = {
+        priority: "high",
+    };
+    // ここで実際に通知を送信している
+    admin.messaging().sendToDevice(token, payload, option);
+};
+
+// 新規依頼作時
+export const createItList = functions
     .region("asia-northeast1")
     .firestore.document("itList/{docId}")
-    .onCreate(async (snapshot) => {
-        // 関数は発火してるのでここまでの記述は問題ないはず
+    .onCreate(async (snapshot, context) => {
+        // ここにitListのデータが入っている(createdAt,ifText,thenText)
+        const itList = snapshot.data();
 
-        // const fcmTokens = [];
-
-        const newIfThen = snapshot.data();
-        // const newIfText = newIfThen.doc("ifText");
-        // const newThenText = newIfThen.data()["thenText"];
-
-        // const tokens = await admin.firestore().collection("users").get();
-        // トークンべた書きも送れなかった
-        const tokens = [
-            "eOHxYMFAHUjguDS1UsTg41:APA91bEaitT55pJFP2MrCh5iaF12_bzh-qVqVbyRNNelKasLyhbuXmuRSGEL1yi-BdNSoM18st8xxg8IDxohDbmmrOAcI-t7fNN5QIUUV4N4IfIWWgXoqd8ZeraaBJSR1xMREdwLvGEB",
-        ];
-
-        // for (const token of tokens.docs) {
-        //     fcmTokens.push(token.data()._tokens);
-        // }
-
-        const payload = {
-            notification: {
-                title: "Push Title",
-                body: "Push Body",
-                sound: "default",
-            },
-            date: {
-                click_action: "FULUTTER_NOTIFICATION_CLICK",
-                message: newIfThen.message,
-            },
-        };
-        try {
-            const response = await admin
-                .messaging()
-                .sendToDevice(tokens, payload);
-            console.log("Notification sent succsessfully", response);
-        } catch (err) {
-            console.log("Error sending Notification.");
-        }
+        const receiverRef = firestore.collection("users").doc(itList["userId"]);
+        // 受信者の情報にアクセスする
+        receiverRef.get().then(function (doc) {
+            if (doc.exists === true) {
+                // 受信者の情報を取得(name,fcmToken)
+                // const receiver = doc.data();
+                const fcmToken =
+                    "eOHxYMFAHUjguDS1UsTg41:APA91bEaitT55pJFP2MrCh5iaF12_bzh-qVqVbyRNNelKasLyhbuXmuRSGEL1yi-BdNSoM18st8xxg8IDxohDbmmrOAcI-t7fNN5QIUUV4N4IfIWWgXoqd8ZeraaBJSR1xMREdwLvGEB";
+                const ifText = itList["ifText"];
+                const thenText = itList["thenText"];
+                // 通知のタイトル
+                const title = ifText;
+                // 通知の内容
+                const body = thenText;
+                sendPushNotification(fcmToken, title, body, "1");
+                console.log("newitList");
+            } else {
+                console.log("notExists");
+            }
+        });
     });

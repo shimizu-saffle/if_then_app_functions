793989eee594adf77469b974bdba2a31791903af
shimizu-saffle <shimizu.saffle@gmail.com>
2021-08-24 20:44:25 +0900

    createItList7test
    デプロイできたけど関数の発火時にエラーになる。異なるトリガーは組み合わせられなさそう

--- functions/src/ifthen_pubsub.ts
+++ functions/src/ifthen_pubsub.ts
@@ -24,30 +24,44 @@ const sendPushNotification = async function (
     await admin.messaging().sendToDevice(token, payload, option);
 };
 
-export const remindIfThenNotificationTest = functions
+export const remindIfThenNotificationTest2 = functions
     .region("asia-northeast1")
     .runWith({ memory: "512MB" })
     .pubsub.schedule("every 2 minutes")
     .timeZone("Asia/Tokyo")
     .onRun(async () => {
-        // user全員に対して通知を送る事を表現する
-        // まずは複数のユーザーにべた書きメッセで通知を送れるかテストする
+        // user全員に対して通知を送るための記述
+        // まずは複数のユーザーにべた書きメッセで通知を送れるかテストする→テスト完了
 
         const pushUsersRef = firestore.collection("users");
+        const AllIfThenInfo = firestore.collection("itList");
 
         const usersSnapshot = await pushUsersRef.get();
+        const itListSnapshot = await AllIfThenInfo.get();
 
         if (usersSnapshot.empty) {
-            console.log("No matching documents.");
+            console.log("No matching　users documents.");
             return;
         }
+        if (itListSnapshot.empty) {
+            console.log("No matching　ifthen documents.");
+            return;
+        }
+
         usersSnapshot.forEach((doc) => {
+            // Functionsのログにユーザーのメールアドレスとトークンが表示されてる
             console.log(doc.id, "=>", doc.data());
             console.log(doc.data()["postAt"]);
-            //通知送信
             const tokens = doc.data()["tokens"];
-            const title = "イフ";
-            const body = "ゼン";
-            sendPushNotification(tokens, title, body, "1");
+
+            itListSnapshot.forEach((doc) => {
+                console.log(doc.id, "=>", doc.data());
+                console.log(doc.data()["postAt"]);
+
+                const title = doc.data()["ifText"];
+                const body = doc.data()["thenText"];
+                // この関数で通知を送信してる
+                sendPushNotification(tokens, title, body, "1");
+            });
         });
     });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,54 +3,53 @@ import * as admin from "firebase-admin";
 admin.initializeApp(functions.config().firebase);
 export const firestore = admin.firestore();
 
-export * from "./ifthen_pubsub";
+// export * from "./ifthen_pubsub";
 
 // プッシュ通知を送る関数
-const sendPushNotification = async function (
-    token: string[],
-    title: string,
-    body: string,
-    badge: string
-) {
-    const payload = {
-        notification: {
-            title: title,
-            body: body,
-            badge: badge,
-            sound: "default",
-        },
-    };
-    const option = {
-        priority: "high",
-    };
-    // ここで実際に通知を送信している
-    await admin.messaging().sendToDevice(token, payload, option);
-};
+const sendPushNotification = functions
+    .region("asia-northeast1")
+    .runWith({ memory: "512MB" })
+    .pubsub.schedule("every 2 minutes")
+    .timeZone("Asia/Tokyo")
+    .onRun(async () => {
+        (token: string[], title: string, body: string, badge: string) => {
+            const payload = {
+                notification: {
+                    body: body,
+                    badge: badge,
+                    sound: "default",
+                },
+            };
+            const option = {
+                priority: "high",
+            };
+            // ここで実際に通知を送信している
+            admin.messaging().sendToDevice(token, payload, option);
+        };
+    });
 
 // 新規依頼作時
-export const createItList6 = functions
+export const createItList7test = functions
     .region("asia-northeast1")
     .firestore.document("itList/{docId}")
+    // context消しても通知届いた上にすぐ通知が届くようになった
     .onCreate(async (snapshot) => {
         // ここにitListのデータが入っている(createdAt,ifText,thenText)
         const itList = snapshot.data();
 
         const receiverRef = firestore.collection("users").doc(itList["userId"]);
-        // const userSnapshot = receiverRef.collection("users").get();
-        // const tokens = (await userSnapshot).docs.map((doc) => doc.id);
-
         receiverRef.get().then(function (doc) {
             if (doc.exists === true) {
                 // usersの情報を取得(name,fcmTokens)
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 const receiver = doc.data()!;
                 const fcmTokens = receiver["tokens"];
                 const ifText = itList["ifText"];
                 const thenText = itList["thenText"];
-                // 通知のタイトル
-                const title = ifText;
+
                 // 通知の内容
-                const body = thenText;
-                sendPushNotification(fcmTokens, title, body, "1");
+                const body = ifText + thenText;
+                sendPushNotification(fcmTokens, body);
                 console.log(ifText + thenText);
             } else {
                 console.error("notExists");
